65176ee8d5487ab3939fc0f7ca0b16e699333077
Yamashita-414 <yamashita_e@cri-me.com>
2026-09-10 19:26:15 +0900

    2026-9-10 JS記述中 kvの画像情報取得まで完了。JS用にHTMLとCSSを少し変更しました。

---
+++ web_step2_山下/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // 1. カルーセルの画像要素をすべて取得する
+    const images = document.querySelectorAll('.p-kv__image');
+    
+    // 2. コンソールに出力して確認！
+    console.log(images);
+});
